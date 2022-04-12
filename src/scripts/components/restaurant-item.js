9534edd8a6cf123597b09ea68adca07c9af06985
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-12 14:07:36 +0700

    change html tag & img alt

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -19,21 +19,21 @@ class RestaurantItem extends HTMLElement {
     this.setAttribute('id', this._restaurant.id);
     this.classList.add('restaurant-item');
     this.innerHTML = `
-      <div class="restaurant-item__header">
+      <section class="restaurant-item__header">
         <img class="restaurant-item__thumbnail" src="${this._restaurant.pictureId}" alt="${this._restaurant.name} thumbnail" width="100%">
         <span class="restaurant-item__rating" title="Rating">
           <i class="fa-solid fa-star" role="img">Rating:</i>
           ${this._restaurant.rating}
         </span>
-      </div>
-      <div class="restaurant-item__content">
+      </section>
+      <section class="restaurant-item__content">
         <p class="restaurant-item__content__name">${this._restaurant.name}</p>
         <div class="restaurant-item__content__city">
           <i class="fa-solid fa-location-dot" role="img">Location:</i>
           <span>${this._restaurant.city}</span>
         </div>
         <p class="restaurant-item__content__description">${this._restaurant.description}</p>
-      </div>
+      </section>
     `;
   }
 }
