1cb19665a46eab59d081dbcebac09ecbe50d63d4
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-16 15:27:16 +0700

    big update on deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,6 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, 'dist'),
   },
 });
