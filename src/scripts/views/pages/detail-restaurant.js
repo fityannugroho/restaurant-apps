524c1c32c902063c6bf32e032781aebbdf1db387
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-26 08:32:58 +0700

    init restaurant detail page
    - Add getRestaurant in restaurant-source.js.
    - Add content of restaurant detail page.
    - Get restaurant data from RestaurantSource in detail-restaurant.js.
    - Add styling for content of restaurant detail page in main.css.
    - Create template-creator.js

--- src/scripts/views/pages/detail-restaurant.js
+++ src/scripts/views/pages/detail-restaurant.js
@@ -1,10 +1,22 @@
+import { createRestaurantDetailTemplate } from '../../../templates/template-creator';
+import RestaurantSource from '../../data/restaurant-source';
+import UrlParser from '../../routes/url-parser';
+
 const DetailRestaurant = {
   async render() {
     return `
+      <div class="content">
+        <section id="restaurant" class="restaurant-detail wrapper"></section>
+      </div>
     `;
   },
 
   async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantSource.getRestaurant(url.id);
+
+    const restaurantContainer = document.querySelector('#restaurant');
+    restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
   },
 };
 
---
+++ src/templates/template-creator.js
@@ -0,0 +1,45 @@
+import RESTAURANT_API_ENDPOINT from '../scripts/globals/restaurant-api-endpoint';
+
+const createCategoryTagsTemplate = (categories) => categories.map((category) => `<span class="restaurant-detail__category">${category.name}</span>`).join('');
+
+const createRestaurantDetailTemplate = (restaurant) => `
+  <img class="restaurant-detail__thumbnail" src="${RESTAURANT_API_ENDPOINT.RESTAURANT_PICTURE('medium', restaurant.pictureId)}">
+  <div class="restaurant-detail__content">
+    <h2 class="restaurant-detail__title">${restaurant.name}</h2>
+    <table class="restaurant-detail__table">
+      <tbody>
+        <tr>
+          <th>
+            <i class="fa-solid fa-star"></i>
+            <span>Rating</span>
+          </th>
+          <td>${(restaurant.rating).toPrecision(2)}</td>
+        </tr>
+        <tr>
+          <th>
+            <i class="fa-solid fa-city"></i>
+            <span>City</span>
+          </th>
+          <td>${restaurant.city}</td>
+        </tr>
+        <tr>
+          <th>
+            <i class="fa-solid fa-location-dot"></i>
+            <span>Address</span>
+          </th>
+          <td>${restaurant.address}</td>
+        </tr>
+        <tr>
+          <th>
+            <i class="fa-solid fa-tag"></i>
+            <span>Tags</span>
+          </th>
+          <td>${createCategoryTagsTemplate(restaurant.categories)}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p class="restaurant-detail__description">${restaurant.description}</p>
+  </div>
+`;
+
+export { createRestaurantDetailTemplate };
