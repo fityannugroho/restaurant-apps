b67c775cc6046394143e943aa647b845c6c37325
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-27 10:02:54 +0700

    init service worker

---
+++ src/scripts/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  // TODO: Caching App Shell Resource
+});
+
+self.addEventListener('activate', (event) => {
+  // TODO: Delete old caches
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+  // TODO: Add/get fetch request to/from caches
+});
