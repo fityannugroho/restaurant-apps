1b1fa0f4c40c4bdc862a0ca56953c88643e448ab
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-27 18:53:44 +0700

    add content of favorite restaurant page

--- src/scripts/views/pages/favorite-restaurants.js
+++ src/scripts/views/pages/favorite-restaurants.js
@@ -1,16 +1,22 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+
 const FavoriteRestaurants = {
   async render() {
     return `
-      <div class="wrapper">
-        <section class="section">
-          <h2 class="section__title">My Favorite</h2>
-          <restaurant-list class="restaurants"></restaurant-list>
-        </section>
+      <div class="content">
+        <div class="wrapper">
+          <section class="section">
+            <h2 class="section__title">My Favorite Restaurant</h2>
+            <restaurant-list class="restaurants"></restaurant-list>
+          </section>
+        </div>
       </div>
     `;
   },
 
   async afterRender() {
+    const restaurantList = document.querySelector('restaurant-list');
+    restaurantList.restaurants = await FavoriteRestaurantIdb.getAll();
   },
 };
 
