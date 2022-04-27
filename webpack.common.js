6c24dc063a3ed1c46a37981e7e2d6590972f632a
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-27 09:01:10 +0700

    init manifest and icon

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -37,6 +38,34 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WebpackPwaManifest({
+      name: 'Fresto | Fityan\'s French Restaurant',
+      short_name: 'Fresto',
+      description: 'Experience French cuisine in a different restaurant scenes',
+      lang: 'en-US',
+      display: 'standalone',
+      orientation: 'portrait',
+      inject: true,
+      ios: true,
+      background_color: '#ffffff',
+      theme_color: '#71c9ce',
+      publicPath: '/',
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: path.resolve('src/public/images/logo/icon-192x192.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: 'any',
+        },
+        {
+          src: path.resolve('src/public/images/logo/icon-192x192.png'),
+          sizes: [57, 60, 72, 76, 114, 120, 144, 152, 180],
+          purpose: 'any',
+          ios: true,
         },
       ],
     }),
