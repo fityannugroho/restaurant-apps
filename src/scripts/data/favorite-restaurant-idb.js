444657465df78f9195fb017c0b782ab9b52017db
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-27 16:27:28 +0700

    create indexed DB functionality

---
+++ src/scripts/data/favorite-restaurant-idb.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+import CONFIG from '../globals/config';
+
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+  },
+});
+
+const FavoriteRestaurantIdb = {
+  async getAll() {
+    const db = await dbPromise;
+    return db.getAll(OBJECT_STORE_NAME);
+  },
+
+  async get(id) {
+    const db = await dbPromise;
+    return db.get(OBJECT_STORE_NAME, id);
+  },
+
+  async put(restaurant) {
+    const db = await dbPromise;
+    return db.put(OBJECT_STORE_NAME, restaurant);
+  },
+
+  async delete(id) {
+    const db = await dbPromise;
+    return db.delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavoriteRestaurantIdb;
