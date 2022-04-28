8a4b66230e63b373759857334e00eedf626b4d42
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-28 13:12:20 +0700

    v0.3.1: remove dish section
    - Remove dish section from home page.
    - Delete dish-item.js.
    - Delete dish-list.js
    - Remove dish-item and dish-list styling from main.css.

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -14,10 +14,6 @@ const Home = {
           <h2 class="section__title">Explore Restaurant</h2>
           <restaurant-list></restaurant-list>
         </section>
-        <section class="section">
-          <h2 class="section__title">Explore Dish</h2>
-          <dish-list class="dishes"></dish-list>
-        </section>
       </div>
     `;
   },
@@ -31,17 +27,6 @@ const Home = {
       restaurantList.renderError('Something went wrong while getting the restaurants.');
     } else {
       restaurantList.restaurants = restaurants;
-    }
-
-    // Render the dish list.
-    const dishList = document.querySelector('dish-list');
-    try {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=French');
-      const { meals } = await response.json();
-      dishList.dishes = meals;
-    } catch (error) {
-      dishList.renderError();
-      console.error(error);
     }
   },
 };
