f403cae9901f8245fb0dacb033950b99f4a3d256
Fityan <fityannugroho@gmail.com>
2022-05-06 20:12:17 +0700

    lazy-loading images with lazysizes
    - Add lazysizes@5.3.2.
    - Implement lazy loading to restaurant item thumbnail.
    - Update version to 0.4.1.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import './components/restaurant-list';
