7a08c96b63ae7c22f8e39c715fa3b6c18ffd9506
Fityan <fityannugroho@gmail.com>
2022-05-09 09:02:21 +0700

    add 'unfavorite restaurant' e2e testing

---
+++ e2e/unfavorite-restaurant.spec.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+Feature('Unfavorite Restaurant');
+
+Before(({I}) => {
+  I.amOnPage('/');
+  I.waitForElement('restaurant-item');
+  I.seeElement('restaurant-item');
+  I.click(locate('restaurant-item').first());
+  I.waitForElement('#favoriteButton');
+  I.seeElement('#favoriteButton');
+  I.click('#favoriteButton');
+  I.amOnPage('/#/favorites');
+});
+
+Scenario('showing a favorited restaurant', ({ I }) => {
+  I.seeElement('restaurant-item');
+});
+
+Scenario('unfavorite a restaurant', async ({ I }) => {
+  I.seeElement('restaurant-item');
+  I.click(locate('restaurant-item').first());
+  I.waitForElement('#favoriteButton');
+  I.seeElement('#favoriteButton[aria-label="Unfavorite this restaurant"]');
+  I.click('#favoriteButton');
+  I.amOnPage('/#/favorites');
+  I.dontSeeElement('restaurant-item');
+});
