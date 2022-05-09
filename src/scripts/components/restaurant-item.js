3bc0667aeec29b945c3a1dcd2b2fa24111b9d912
Fityan <fityannugroho@gmail.com>
2022-05-10 06:57:15 +0700

    improve restaurant item ui
    - Fix restaurant item thumbnail styling.
    - Change the placeholder of restaurant item description.
    - Increase the amount of restaurant item skeleton.

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -52,7 +52,7 @@ class RestaurantItem extends HTMLElement {
           <i class="fa-solid fa-location-dot"></i>
           <p title="Location">City...</p>
         </div>
-        <p class="restaurant-item__content__description">Description...</p>
+        <p class="restaurant-item__content__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
       </section>
     `;
   }
